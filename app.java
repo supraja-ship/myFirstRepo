786982e3f74124891f1e4d5854f3363419ac6851
Supraja <78201241+supraja-ship@users.noreply.github.com>
2025-08-02 17:00:07 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,3 +1,22 @@
 helo this is my first java program
 public static void main(string args[])
 	dont't forget to create the clas
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
+}
+Output
